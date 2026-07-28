399a0700501dad754ee12a63e118a5bbd350c348
martin-henz <martin-henz@users.noreply.github.com>
2026-07-28 11:17:12 +0000

    deploy: source-academy/modules@f1c24876d4adfbca8311b111c73b5d1c3b446e70

---
+++ devdocs/assets/chunks/swimlanesDiagram-G3AALYLV.DwdGy5jU.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.MLphdyft.js";import{_ as a}from"../app.lzeAPWUz.js";import"./chunk-5VM5RSS4.DoMzc0-9.js";import"./chunk-XXDRQBXY.BmdDGGlI.js";import"./chunk-VR4S4FIN.S-eEkkxo.js";import"./chunk-32BRIVSS.BMiN-Dn9.js";import"./channel.B0mocTEe.js";import"./framework.DuwpaNh0.js";import"./theme.Ds7onL1A.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
--- devdocs/assets/chunks/swimlanesDiagram-G3AALYLV.NjbCaRnr.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.Dty-MfQX.js";import{_ as a}from"../app.QzRKYSh-.js";import"./chunk-5VM5RSS4.D3QsR5HJ.js";import"./chunk-XXDRQBXY.Dfcd7OB1.js";import"./chunk-VR4S4FIN.D67ltbAl.js";import"./chunk-32BRIVSS.hCZ1Lucy.js";import"./channel.DLTXggwe.js";import"./framework.DuwpaNh0.js";import"./theme.BxyoO30F.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
