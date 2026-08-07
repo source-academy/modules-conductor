0fcf44da12fab6c3bbcdecceb228926aeecb879f
martin-henz <martin-henz@users.noreply.github.com>
2026-08-07 02:14:08 +0000

    deploy: source-academy/modules@2d46e8a18673b88e12925860e2cdb6423157972d

--- devdocs/assets/chunks/swimlanesDiagram-ULZ7WXOC.ChZ04o7k.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DkDFM8-Z.js";import{_ as a}from"../app.BvcxYxDS.js";import"./chunk-5VM5RSS4.STuSBjwY.js";import"./chunk-XXDRQBXY.C2AT5HfY.js";import"./chunk-KBJHAD2P.j-gEyErM.js";import"./chunk-2GRJ4B5K.YUQYm9CL.js";import"./channel.Bn5Ea0KE.js";import"./framework.DuwpaNh0.js";import"./theme.CBwlDN3R.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
