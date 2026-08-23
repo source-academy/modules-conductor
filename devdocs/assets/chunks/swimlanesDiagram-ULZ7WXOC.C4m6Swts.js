179a0180923b7b721e1fa051aea3296c0e8d492f
RichDom2185 <RichDom2185@users.noreply.github.com>
2026-08-23 22:45:54 +0000

    deploy: source-academy/modules@cf48abd74bca1b0894f8ee0a0cb03cb6ad3618e7

---
+++ devdocs/assets/chunks/swimlanesDiagram-ULZ7WXOC.C4m6Swts.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.COu--b8f.js";import{_ as a}from"../app.Cqz26hRC.js";import"./chunk-5VM5RSS4.BHyVm3Mn.js";import"./chunk-XXDRQBXY.mCwucNIh.js";import"./chunk-KBJHAD2P.BZOoZhqq.js";import"./chunk-2GRJ4B5K.ClPaUauC.js";import"./channel.Buq-kHDl.js";import"./framework.DuwpaNh0.js";import"./theme.BG4zSPwX.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
