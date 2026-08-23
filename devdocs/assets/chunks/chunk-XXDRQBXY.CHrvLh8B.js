8a0349cbfa8324581971161d0b12675b14790ec1
RichDom2185 <RichDom2185@users.noreply.github.com>
2026-08-23 22:49:11 +0000

    deploy: source-academy/modules@06f8548d61d5867020039dd749bb07e7c4a243da

---
+++ devdocs/assets/chunks/chunk-XXDRQBXY.CHrvLh8B.js
@@ -0,0 +1,1 @@
+import{_ as a,d as o}from"../app.BiT6E3FW.js";var d=a((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
---
+++ devdocs/assets/chunks/swimlanesDiagram-ULZ7WXOC.NgDY66mU.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.BLyXCPyF.js";import{_ as a}from"../app.BiT6E3FW.js";import"./chunk-5VM5RSS4.BkT-c8ea.js";import"./chunk-XXDRQBXY.CHrvLh8B.js";import"./chunk-KBJHAD2P.Bn9_7vsG.js";import"./chunk-2GRJ4B5K.DX6OKPeU.js";import"./channel.FNx__Gva.js";import"./framework.DuwpaNh0.js";import"./theme.DAW_Dtg_.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
